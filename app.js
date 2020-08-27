b50175fb436c7597c998f3278878c9e809ff6ec8
Oscar Erazo| <erazom23@hotmail.com>
2020-08-26 20:53:07 -0500

    maquetamos y dimos estilo al componente. Falta funcion que despliegue componentes

---
+++ app.js
@@ -0,0 +1,16 @@
+var x = document.getElementById('sharing')
+var y = document.getElementById('profile')
+
+var share1 = document.getElementById('share1')
+var share2 = document.getElementById('share2')
+
+console.log(x)
+console.log(y);
+
+x.style.display="flex";
+y.style.display="none";
+
+share1.addEventListener('click')
+share2.addEventListener('click')
+
+
