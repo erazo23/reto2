2e65a40dcb2ad4fb3270d1d348b45089a2eaf97e
Oscar Erazo| <erazom23@hotmail.com>
2020-08-27 18:25:08 -0500

    se agrega funcion para cambiar componentes

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-var x = document.getElementById('sharing')
-var y = document.getElementById('profile')
+var x = document.getElementById("profile")
+var y = document.getElementById("sharing")
 
-var share1 = document.getElementById('share1')
-var share2 = document.getElementById('share2')
+var share1 = document.getElementById("share1")
+var share2 = document.getElementById("share2")
 
 console.log(x)
 console.log(y);
 
-x.style.display="flex";
-y.style.display="none";
+console.log(share1);
+console.log(share2);
 
-share1.addEventListener('click')
-share2.addEventListener('click')
+
+function compartir1(j,k) {
+    
+    j.style.display="none";
+    k.style.display="flex";
+}
+
+function compartir2(j,k) {
+    j.style.display="flex";
+    k.style.display="none";
+}
+
+share1.onclick = function (x,y) {
+    compartir1()
+};
+
+share2.onclick = function (x,y) {
+    compartir2()
+};
+
+// share2.onclick = compartir2(this.x,this.y);
 
 
