a425d1d34606f01b97b3369dd2c20f9ddcd0205d
Oscar Erazo| <erazom23@hotmail.com>
2020-08-28 15:28:12 -0500

    funcion de cambiar componentes funciona

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
-var x = document.getElementById("profile")
-var y = document.getElementById("sharing")
-
-var share1 = document.getElementById("share1")
-var share2 = document.getElementById("share2")
-
-console.log(x)
-console.log(y);
-
-console.log(share1);
-console.log(share2);
-
-
-function compartir1(j,k) {
-    
-    j.style.display="none";
-    k.style.display="flex";
+document.getElementById("profile").onclick = () => {
+    document.getElementById("profile").style.display = "none";
+    document.getElementById("sharing").style.display = "flex";
 }
 
-function compartir2(j,k) {
-    j.style.display="flex";
-    k.style.display="none";
+document.getElementById("sharing").onclick = () => {
+    document.getElementById("sharing").style.display = "none";
+    document.getElementById("profile").style.display = "flex";
 }
-
-share1.onclick = function (x,y) {
-    compartir1()
-};
-
-share2.onclick = function (x,y) {
-    compartir2()
-};
-
-// share2.onclick = compartir2(this.x,this.y);
-
 
